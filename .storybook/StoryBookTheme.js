30a927f4ea4fc997ee341b8760b7fdd0699b05ce
Shaul Ben david <Shaulbd9@gmail.com>
2024-04-05 11:16:06 +0300

    feat(storybook): update storybook colors

--- .storybook/StoryBookTheme.js
+++ .storybook/StoryBookTheme.js
@@ -8,27 +8,27 @@ export default create({
   brandImage: Logo,
   brandTarget: "_self",
   //
-  colorPrimary: "#810CA8",
-  colorSecondary: "#E5B8F4",
+  colorPrimary: "#8f0d0d",
+  colorSecondary: "#ff5b5b",
 
   // UI
   appBg: "#ffffff",
   appContentBg: "#ffffff",
-  appBorderColor: "#E5B8F4",
+  appBorderColor: "#ff5b5b",
   appBorderRadius: 4,
 
   // Text colors
-  textColor: "#1D0325",
+  textColor: "#0f0f0f",
   textInverseColor: "#ffffff",
 
   // Toolbar default and active colors
-  barTextColor: "#A46CB7",
-  barSelectedColor: "#E5B8F4",
+  barTextColor: "#8f0d0d",
+  barSelectedColor: "#ff5b5b",
   barBg: "#ffffff",
 
   // Form colors
   inputBg: "#ffffff",
-  inputBorder: "#1D0325",
-  inputTextColor: "#1D0325",
+  inputBorder: "#0f0f0f",
+  inputTextColor: "#0f0f0f",
   inputBorderRadius: 2,
 });
--- src/components/Layout/SideMenu/SideMenu.tsx
+++ src/components/Layout/SideMenu/SideMenu.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { FaYoutube } from "react-icons/fa";
 import { Routes } from "@/src/routes";
 import theme from "@/src/styles/tailwind.theme";
-import { Navbar } from "./Navbar";
 import { WEBSITE_URL } from "@/src/constants";
+import { Navbar } from "./Navbar";
 
 export const SideMenu = (): JSX.Element => (
   <aside className="flex h-full w-64 flex-col border-r border-white/30 p-4">
