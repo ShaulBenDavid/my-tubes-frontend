b614da910b94f110864c96971e28259d4b2f0e14
Shaul Ben david <Shaulbd9@gmail.com>
2024-06-15 17:17:15 +0300

    feat(drawer): new component

---
+++ src/components/Drawer/Drawer.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "test-utils";
+import { Drawer } from "./Drawer";
+
+test("Drawer - should render element with aria-hidden false", () => {
+  render(<Drawer isOpen onClose={() => ({})} />);
+  const element = screen.getByTestId("drawer");
+
+  expect(element).toHaveAttribute("aria-hidden", "false");
+});
