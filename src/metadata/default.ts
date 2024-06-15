419edc5882eaec6edec7fc7a83bf854ed75e0662
Shaul Ben david <Shaulbd9@gmail.com>
2024-06-15 11:25:33 +0300

    fix(seo): naming

--- src/metadata/default.ts
+++ src/metadata/default.ts
@@ -6,7 +6,7 @@ const url = new URL("https://my-tubes.com");
 export const defaultMetadata: Metadata = {
   title: {
     default:
-      "My-Yubes - Manage Your YouTube Channels and Get Detailed Statistics",
+      "My-Tubes - Manage Your YouTube Channels and Get Detailed Statistics",
     template: "%s | My-Tubes",
   },
   description:
@@ -55,7 +55,7 @@ export const defaultMetadata: Metadata = {
   },
   openGraph: {
     title:
-      "My-Yubes - Manage Your YouTube Channels and Get Detailed Statistics",
+      "My-Tubes - Manage Your YouTube Channels and Get Detailed Statistics",
     description:
       "My-Tubes is a comprehensive tool for managing your YouTube channels, following other channels, and accessing a detailed statistics dashboard. Optimize your content and track your growth with ease.",
     url: WEBSITE_URL,
@@ -71,7 +71,7 @@ export const defaultMetadata: Metadata = {
   },
   twitter: {
     title:
-      "My-Yubes - Manage Your YouTube Channels and Get Detailed Statistics",
+      "My-Tubes - Manage Your YouTube Channels and Get Detailed Statistics",
     site: "@mytubes",
     description:
       "My-Tubes is a comprehensive tool for managing your YouTube channels, following other channels, and accessing a detailed statistics dashboard. Optimize your content and track your growth with ease.",
