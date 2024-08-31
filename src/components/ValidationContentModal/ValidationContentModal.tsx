03ff5a3e693b28428928d2c0da866df041d3ed36
Shaul Ben david <Shaulbd9@gmail.com>
2024-08-31 11:58:54 +0300

    fix(deletemodal): responsive to mobile

--- src/components/ValidationContentModal/ValidationContentModal.tsx
+++ src/components/ValidationContentModal/ValidationContentModal.tsx
@@ -18,7 +18,7 @@ export const ValidationContentModal = ({
   onClose,
   onDelete,
 }: ValidationContentModalProps): JSX.Element => (
-  <div className="flex h-fit w-96 flex-col items-center justify-center">
+  <div className="flex h-fit w-72 flex-col items-center justify-center tb:w-96">
     <div className="flex flex-col items-center">
       <IoWarningOutline size={60} color={theme.primary[500]} />
       <h4 className="text-lg font-semibold text-primary-500">{title}</h4>
