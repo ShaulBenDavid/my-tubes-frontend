919bde3a2e04f19dde411dc39c42e7060029a903
Shaul Ben david <Shaulbd9@gmail.com>
2024-10-03 10:20:26 +0300

    fix(layout): app layout css

--- app/(user)/(private)/layout.tsx
+++ app/(user)/(private)/layout.tsx
@@ -19,25 +19,23 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <ProtectedRoute>
       <div className="flex h-full flex-1 flex-row justify-start">
-        <aside className="hidden h-full w-72 flex-col border-r border-white/30 p-4 lg:flex">
+        <aside className="hidden h-full w-72 shrink-0 flex-col border-r border-white/30 p-4 lg:flex">
           <SideMenu />
         </aside>
-        <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col overflow-hidden">
           <Header onClick={toggleDrawer} />
           <main className="w-full flex-1 justify-center overflow-hidden p-2 tb:p-4">
             {children}
           </main>
-          <div className="visible lg:hidden">
-            <Drawer
-              onClose={toggleDrawer}
-              isOpen={showDrawer}
-              id={SIDE_DRAWER_ARIA}
-            >
-              <div className="flex h-full w-72  flex-col">
-                <SideMenu onClick={toggleDrawer} />
-              </div>
-            </Drawer>
-          </div>
+          <Drawer
+            onClose={toggleDrawer}
+            isOpen={showDrawer}
+            id={SIDE_DRAWER_ARIA}
+          >
+            <div className="flex h-full w-72  flex-col">
+              <SideMenu onClick={toggleDrawer} />
+            </div>
+          </Drawer>
         </div>
       </div>
     </ProtectedRoute>
