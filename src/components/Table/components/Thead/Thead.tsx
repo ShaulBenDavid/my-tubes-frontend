e8c3b033b145cf0959b0380f84685e272b86b6dc
Shaul Ben david <Shaulbd9@gmail.com>
2024-11-02 14:41:24 +0200

    fix(scroll): accross the app

--- src/components/Table/components/Thead/Thead.tsx
+++ src/components/Table/components/Thead/Thead.tsx
@@ -12,8 +12,8 @@ export const Thead = <T extends TableRowType>({
   <thead className="h-10 min-h-[40px] shrink-0 bg-white/10 text-lg">
     <tr className="overflow-hidden text-left">
       {columns.map(({ accessor, header, infoTooltip }) => (
-        <th key={`th-${String(accessor)}`} className="px-2 tb:px-4">
-          <div className="flex flex-row items-center gap-2">
+        <th key={`th-${String(accessor)}`} className="h-10 px-2 tb:px-4">
+          <div className="flex h-full flex-row items-center gap-2">
             {infoTooltip && <InfoTooltip title={infoTooltip} />}
             <span className="truncate capitalize">{header}</span>
           </div>
