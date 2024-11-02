54155dfb0a88bda76b0746e4deed03ea549eb1b0
Shaul Ben david <Shaulbd9@gmail.com>
2024-11-02 14:57:06 +0200

    feat(profile): share profile section

--- src/components/Share/Share.tsx
+++ src/components/Share/Share.tsx
@@ -32,7 +32,7 @@ export const Share = ({
   tooltipContent,
 }: ShareProps): JSX.Element => (
   <div className="flex w-full flex-col gap-4">
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row gap-2">
       <h3 className="text-base font-semibold">Share with your friends</h3>
       <InfoTooltip title={tooltipContent} />
     </div>
--- src/features/Settings/Profile/Profile.tsx
+++ src/features/Settings/Profile/Profile.tsx
@@ -3,6 +3,9 @@ import Skeleton from "react-loading-skeleton";
 import { useGetUserProfile } from "@/src/api/user/hooks";
 import { ProfileForm } from "./components/ProfileForm";
 import { SettingsContent } from "../SettingsLayout";
+import { Share, ShareLoader } from "@/src/components/Share";
+
+const PUBLIC_SHARED_GROUP_URL = "https://my-tubes.com/";
 
 export const Profile = (): JSX.Element => {
   const { isUserLoading, userProfile } = useGetUserProfile();
@@ -13,6 +16,16 @@ export const Profile = (): JSX.Element => {
       description="Manage what others can see about you and link your social accounts for
           easy access."
     >
+      {isUserLoading && <ShareLoader />}
+      {userProfile?.isPublic && !isUserLoading && (
+        <Share
+          url={`${PUBLIC_SHARED_GROUP_URL}@${userProfile.username}/`}
+          title={`${userProfile.username} - MyTubes`}
+          content={`${userProfile.username} Invites You to Explore his Subscriptions..`}
+          tooltipContent="This link allows you to share your profile data and your public groups."
+        />
+      )}
+      <hr className="border-r-2 border-white/30" />
       {isUserLoading && <Skeleton width="full" height="20px" count={5} />}
       {!isUserLoading && userProfile && (
         <ProfileForm defaultForm={userProfile} />
