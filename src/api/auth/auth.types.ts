a645bc4b7b3d9159dd5355979d8ad74bb5160717
Shaul Ben david <Shaulbd9@gmail.com>
2025-01-25 12:12:15 +0200

    feat(role): adding support in roles

--- src/api/auth/auth.types.ts
+++ src/api/auth/auth.types.ts
@@ -1,6 +1,12 @@
+export enum Roles {
+  CREATOR = "creator",
+  USER = "user",
+}
+
 export type AuthResponseType = {
   accessToken: string;
   googleToken: string;
+  role: Roles;
 };
 
 export type LoginParamsType = {
